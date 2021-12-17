ee6b0bbc69f7960b68f1ce3add260ab6a4685bd1
Baihaki Al Biruni <baihakitanjung12@gmail.com>
2021-12-17 14:40:09 +0700

    fix: socket layout

--- vue3-socket/src/App.vue
+++ vue3-socket/src/App.vue
@@ -1,60 +1,13 @@
 <template>
-  <div>
-    <div class="product-card bg-yellow-300 items-center h-screen">
-      <div class="flex justify-center p-5">
-        <!-- <div class="mr-3" v-if="user">
-          <img width="50" height="50" :src="user.photoURL" alt="fotoGmail" />
-        </div> -->
-        <!-- <button
-          v-if="!isLogin"
-          @click="signIn"
-          class="
-            px-10
-            py-4
-            bg-blue-400
-            text-white text-xs
-            font-bold
-            uppercase
-            rounded
-          "
-        >
-          Sign In
-        </button>
-        <button
-          v-else
-          @click="signOut"
-          class="
-            px-10
-            py-4
-            bg-blue-400
-            text-white text-xs
-            font-bold
-            uppercase
-            rounded
-          "
-        >
-          Sign Out
-        </button> -->
-      </div>
-      <div class="flex p-1 justify-around">
-        <!-- <template v-for="data in data.cars" :key="data.id"> -->
-        <ProductCard />
-        <!-- </template> -->
-      </div>
-    </div>
+  <div class="text-center">
+    <h1>SEE CONSOLE LOG</h1>
   </div>
 </template>
 
 <script>
-// import HelloWorld from "./components/HelloWorld.vue";
-import ProductCard from "./components/ProductCard.vue";
 
 export default {
   name: "App",
-  components: {
-    // HelloWorld,
-    ProductCard
-  },
   mounted() {
     window.Echo.channel("EveryoneChannel").listen(".EveryoneMessage", function(
       e
